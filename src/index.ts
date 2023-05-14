d3c1f3b37da088aa355fb7e69fb3179616bc6b00
eihigh <eihigh.contact@gmail.com>
2023-05-14 20:43:56 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './index.css';
 import { h, text, patch } from "superfine"
 const v = (t: string, p = {}) => (...c: any[]) => h(t, p, c); // another syntax of h
 const u = (f: { (): void }) => () => { f(); update(); }; // update() after calling f
@@ -5,7 +6,7 @@ const u = (f: { (): void }) => () => { f(); update(); }; // update() after calli
 let n = ["あくたー", "じょぶ", "しすてむ"];
 
 function doClick() {
-	// n.push(n.length);
+	n.push(n.length);
 }
 
 function sidebar() {
